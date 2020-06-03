48c5868d324119336e31cc92ade367260d69c575
Victor Gazolli <victorlpgazolli@gmail.com>
2020-06-02 21:24:18 -0300

    Fixed high and low variables names

--- src/main.asm
+++ src/main.asm
@@ -3,16 +3,16 @@
 .nolist
 .list
 .org 0x0000
-.def high = r16
-.def low = r17
+.def on = r16
+.def off = r17
 .def ldr = r18
 	rjmp start
 start:
-	ldi high, 0xff
-	ldi low, 0x00
-	out ddrb, high
-	out ddrd, low
-	out portb, low
+	ldi on, 0xff
+	ldi off, 0x00
+	out ddrb, on
+	out ddrd, off
+	out portb, off
 	rjmp loop
 
 loop:
@@ -21,5 +21,5 @@ loop:
 	brlo led
 	rjmp loop
 led:	
-	out portb, high
+	out portb, on
 	rjmp loop
