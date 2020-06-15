91861428602ea37f6effc4b43c68b2efc59317ae
Victor Gazolli <victorlpgazolli@gmail.com>
2020-06-15 09:04:36 -0300

    Changed left ajusted to right ajusted

--- src/main.asm
+++ src/main.asm
@@ -19,7 +19,7 @@ loop:
     call wait_adc
     lds adclow, ADCL 		; Ler o ADCL antes do ADCH por ser o low byte do adc
     lds adchigh, ADCH
-	cpi adchigh, 0x80 
+	cpi adchigh, 0x02 
 	brsh led_off		; se o ADCH > 0x80 desliga o led
 	out portb, on		; senão liga o led
 	rjmp loop
@@ -29,7 +29,7 @@ led_off:
 	rjmp loop
 
 configure_adc:
-    ldi r22, 0b01100000   ; os ultimos 3 bits seleciona o canal ADC0 como pino de entrada
+    ldi r22, 0b01000000   ; os ultimos 3 bits seleciona o canal ADC0 como pino de entrada
     sts ADMUX, r22
 
     ldi r22, 0b10000101   ; Habiliita o ADC e seleciona o modo "Single Conversion"
